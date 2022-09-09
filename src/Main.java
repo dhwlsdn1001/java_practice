8da3e146bf0a952cc7324d6944eafefc354b23ba
dhwlsdn1001 <ojw061001@naver.com>
2022-09-09 14:40:06 +0900

    22/9/9-1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello, World!");
+        System.out.println("java_practice");
     }
 }
