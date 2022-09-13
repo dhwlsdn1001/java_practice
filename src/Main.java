b7a1535f1b896b885b42d26418a5e1bf3a077829
dhwlsdn1001 <ojw061001@naver.com>
2022-09-14 08:41:28 +0900

    22/9/14-1(반복제어문3-자가진단1)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Hello, World!");
+        int a = sc.nextInt();
+        int sum = 0;
+        int i = 1;
+        while(true) {
+            sum += i;
+            if(sum >= a) {
+                break;
+            }
+            i += 2;
+        }
+        System.out.printf("%d %d", (i + 1) / 2, sum);
     }
 }
