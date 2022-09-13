476e18c368c565e8f1569c952ff90516e43d8d9c
dhwlsdn1001 <ojw061001@naver.com>
2022-09-14 08:55:54 +0900

    22/9/14-4(반복제어문3-자가진단4)

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         for(int i = 1; i <= a; i++) {
+            for(int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
             for(int j = a; j >= i; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        for(int i = 1; i <= a; i++) {
-            for(int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
     }
 }
