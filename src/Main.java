f30c7d5af8911e635b98579575e90f33b4106fd9
dhwlsdn1001 <ojw061001@naver.com>
2022-09-14 08:52:25 +0900

    22/9/14-3(반복제어문3-자가진단3)

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        for(int i = 0; i < a; i++) {
-            for(int j = 0; j <= i; j++) {
+        for(int i = 1; i <= a; i++) {
+            for(int j = a; j >= i; j--) {
                 System.out.print("*");
             }
             System.out.println();
         }
+        for(int i = 1; i <= a; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
