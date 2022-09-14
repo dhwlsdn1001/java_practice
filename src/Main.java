3773b302d91098b4b08d903a4fcf86d774c35832
dhwlsdn1001 <ojw061001@naver.com>
2022-09-14 19:31:12 +0900

    22/9/14-10(정올-문자열2-자가진단2)

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-        String a[] = {"a", "b", "c", "d", "e"};
-        for(int i = 0 ; i < 5; i++) {
-            a[i] = sc.nextLine();
-        }
-        for(int i = 4; i >= 0; i--) {
-            System.out.println(a[i]);
+        String a;
+        a = sc.nextLine();
+        String[] ap = a.split("\\s");
+        for(int i = 0; i < ap.length; i++) {
+            if(i * 2 < ap.length) {
+                System.out.println(ap[i * 2]);
+            }
         }
     }
 }
