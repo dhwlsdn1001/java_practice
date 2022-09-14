7e309b70dec1097714e42e37f7278d0e6b6ef859
dhwlsdn1001 <ojw061001@naver.com>
2022-09-14 19:13:42 +0900

    22/9/14-7(정올-함수3-자가진단1)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        Print(num1);
+    }
+
+    static int Print(int a) {
+        if(a <= 0) {
+            return 0;
+        } else {
+            System.out.println("recursive");
+            a--;
+            return Print(a);
+        }
     }
 }
